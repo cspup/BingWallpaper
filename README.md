9b5218e34d51c04474fdebf8f4093926f00d3fee
csp <yemuc@qq.com>
2022-02-28 14:13:10 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# 一个java小程序，用来获取必应每日壁纸。
-+ Bing接口 https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1 ,可以获取idx天前n张图片，图片仅可用于桌面壁纸。  
-+ 核心代码是GetBingWallper.java,用来根据bing接口获取每日图片或获取最近7天图片,可设置存储路径，不设置则存在运行路径下。   
-+ BingWallpape.jar是带有简单GUI的客户端，从http://wallpaper.yemuc.xyz 获取图片。  
-# 还写了一个api接口
-+ 网址： http://api.wallpaper.yemuc.xyz/
-+ 请求方式：GET
-+ 响应内容：json字符串 例：{"num":2,"photo":[{"name":"20190501.jpg","url":"https://wallpaper.yemuc.xyz/20190501.jpg"},{"name":"20190502.jpg","url":"https://wallpaper.yemuc.xyz/20190502.jpg"}]}
-+ 参数说明：day--要获取图片的起始日期 num--获取图片张数
-+ 例：获取2019年5月1号之前的5张图片 http://api.wallpaper.yemuc.xyz/?day=20190501&num=5
-+ 目前可以得到2019年3月3日之后的图片。
+# 获取必应每日壁纸工具
++ Bing接口 https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1 ,可以获取idx天前n张图片。  
+
--- src/main/java/com/csp/bingwallpaper/controller/BingWallpaperController.java
+++ src/main/java/com/csp/bingwallpaper/controller/BingWallpaperController.java
@@ -1,13 +1,17 @@
 package com.csp.bingwallpaper.controller;
 
 import com.csp.bingwallpaper.Service.BingWallpaperService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author csp
@@ -35,8 +39,31 @@ public class BingWallpaperController {
 
     @RequestMapping(value = "/getWallpaper",method = RequestMethod.GET)
     @ResponseBody
-    public String get(String fileName){
-        return bingWallpaperService.getWallpaperUPath(fileName);
+    public String getWallpaper(String day, int num) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        Date date = simpleDateFormat.parse(day);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        HashMap<String,Object> result = new HashMap<>();
+        List<String> urlList = new ArrayList<>();
+
+        for (int i=0;i<num;i++){
+            calendar.add(Calendar.DATE, -num);
+            urlList.add("/img/"+simpleDateFormat.format(calendar.getTime())+".jpg");
+        }
+        result.put("num",num);
+        result.put("url",urlList);
+
+
+        String res = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            res = objectMapper.writeValueAsString(result);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 
 
